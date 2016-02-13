ab8b8c1ef6d222a98a9754fa50d3d9b624324fc1
tchewik <elenachistov@gmail.com>
2016-02-13 14:23:02 +0300

    day6

--- js/script.js
+++ js/script.js
@@ -1,49 +1,5 @@
-// Часы в заголовке, обновление раз в десятую секунды
-setInterval(function (){
-	var now = new Date();
-	var hour = now.getHours();
-	var minute = now.getMinutes();
-	var second = now.getSeconds();
-	$("#currenttime").html("Для заблудших душ: " + Date());
-}, 100); 
-
-// Изменение цвета оформления, предметов в навигации, заголовков дня
-function setColorsSubjectsETC(todayis){
-	var nameOfDay = {
-		muladhara: ["Понедельник", ["Диффуры", "Физика", "Философия"]],
-		svadhishthana: ["Вторник", ["Компьютерные сети", "Логическое программирование"]],
-		manipura: ["Среда", ["Java", "Естествознание"]],
-		anahata: ["Четверг", ["Теория вероятностей", "Компьютерные сети", "Научное программирование"]],
-		vishuddha: ["Пятница"],
-		ajna: ["Суббота"],
-		sahasrara: ["Воскресенье"]	
-	}
-	$("title, #title").html(nameOfDay[todayis][0]);
-	$("body").addClass(todayis);
-	var navbarcontent = "";
-	for (i = 0; i < nameOfDay[todayis][1].length; i++){
-		navbarcontent += '<li><a id="linktosection' + (i+1) + '" href="#section' + (i+1) + '">' + nameOfDay[todayis][1][i] + '</a></li>';
-	}
-	$("#subNavBar").html(navbarcontent);
-
-}
-
 $(document).ready(function(){
-	$("#linktosection1").click(function(){
-	$('html, body').stop().animate({
-		scrollTop: $("#section1").offset().top
-	}, 300)
-		})
-	$("#linktosection2").click(function(){
-		$('html, body').stop().animate({
-			scrollTop: $("#section2").offset().top
-			}, 300)
-		})
-	$("#linktosection3").click(function(){
-		$('html, body').stop().animate({
-			scrollTop: $("#section3").offset().top
-		}, 300)
-	})		
+	
 
 	var iswayhere = false;
 	$("#wayspan").click(function(){
